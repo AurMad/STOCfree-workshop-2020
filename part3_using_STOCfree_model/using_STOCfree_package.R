e8d23035471c79a29c6417e637a6eb713fdebabd
Aurélien Madouasse <aurelien.madouasse@oniris-nantes.fr>
2020-11-11 10:02:38 +0100

    Presentation updated

--- part3_using_STOCfree_model/using_STOCfree_package.R
+++ part3_using_STOCfree_model/using_STOCfree_package.R
@@ -1,8 +1,8 @@
-## ---- include = FALSE------------------------------------------------------------------------
+## ---- include = FALSE-------------------------------------------------------------------------
 knitr::opts_chunk$set(cache = TRUE)
 
 
-## ----installing the STOCfree package, eval = FALSE-------------------------------------------
+## ----installing the STOCfree package, eval = FALSE--------------------------------------------
 ## ## attaching the devtools package
 ## library(devtools)
 ## 
@@ -10,23 +10,23 @@ knitr::opts_chunk$set(cache = TRUE)
 ## install_github("AurMad/STOCfree")
 
 
-## ----packages, message = FALSE---------------------------------------------------------------
+## ----packages, message = FALSE----------------------------------------------------------------
 library(STOCfree)
 library(tidyverse)
 library(ggplot2)
 
 
-## ----herdBTM data----------------------------------------------------------------------------
+## ----herdBTM data-----------------------------------------------------------------------------
 head(herdBTM)
 
 
-## ----creation of bvd dataset-----------------------------------------------------------------
+## ----creation of bvd dataset------------------------------------------------------------------
 bvd <- herdBTM %>%
   filter(Test == 'BTM_ODR') %>%
   select(Farm, DateOfTest, ODR, TestResult, LocalSeroPrev)
 
 
-## ----building STOCfree_data object-----------------------------------------------------------
+## ----building STOCfree_data object------------------------------------------------------------
 sfd <- STOCfree_data(
   test_data = bvd,
   test_herd_col = "Farm",
@@ -35,31 +35,31 @@ sfd <- STOCfree_data(
   test_level = "herd")
 
 
-## --------------------------------------------------------------------------------------------
+## ---------------------------------------------------------------------------------------------
 str(sfd, max.level = 1)
 
 
-## ----STOCfree_data var_names-----------------------------------------------------------------
+## ----STOCfree_data var_names------------------------------------------------------------------
 sfd$var_names
 
 
-## ----STOCfree_data herd_id_corresp, eval = FALSE---------------------------------------------
+## ----STOCfree_data herd_id_corresp, eval = FALSE----------------------------------------------
 ## sfd$herd_id_corresp
 
 
-## ----STOCfree_data herd_id_corresp_1, echo = FALSE-------------------------------------------
+## ----STOCfree_data herd_id_corresp_1, echo = FALSE--------------------------------------------
 sfd$herd_id_corresp[1:10,]
 
 
-## ----STOCfree_data test_data, eval = FALSE---------------------------------------------------
+## ----STOCfree_data test_data, eval = FALSE----------------------------------------------------
 ## sfd$herd_test_data
 
 
-## ----STOCfree_data test_data_1, echo = FALSE-------------------------------------------------
+## ----STOCfree_data test_data_1, echo = FALSE--------------------------------------------------
 sfd$test_data[1:10,]
 
 
-## ----extract data herd i, eval = FALSE-------------------------------------------------------
+## ----extract data herd i, eval = FALSE--------------------------------------------------------
 ## ## for herd_id i, Farm is
 ## i <- 1
 ## herd_id_i <- sfd$herd_id_corresp$Farm[sfd$herd_id_corresp$herd_id == i]
@@ -70,47 +70,47 @@ sfd$test_data[1:10,]
 ##   View()
 
 
-## ----herd_test_data, eval = FALSE------------------------------------------------------------
+## ----herd_test_data, eval = FALSE-------------------------------------------------------------
 ## sfd$herd_test_data
 
 
-## ----herd_test_data_1, echo = FALSE----------------------------------------------------------
+## ----herd_test_data_1, echo = FALSE-----------------------------------------------------------
 sfd$herd_test_data[1:10,]
 
 
-## --------------------------------------------------------------------------------------------
+## ---------------------------------------------------------------------------------------------
 sfd$test_perf_prior
 
 
-## ----install betadistapp, eval = FALSE-------------------------------------------------------
+## ----install betadistapp, eval = FALSE--------------------------------------------------------
 ## devtools::install_github("AurMad/betadistapp")
 
 
-## ----run shiny_beta, eval = FALSE------------------------------------------------------------
+## ----run shiny_beta, eval = FALSE-------------------------------------------------------------
 ## betadistapp::shiny_beta()
 
 
-## --------------------------------------------------------------------------------------------
+## ---------------------------------------------------------------------------------------------
 sfd <- set_priors_tests(sfd,
                  Se_a = 5000,
                  Se_b = 260,
-                 Sp_a = 20,
-                 Sp_b = 2)
+                 Sp_a = 9000,
+                 Sp_b = 1000)
 
 
-## --------------------------------------------------------------------------------------------
+## ---------------------------------------------------------------------------------------------
 show_tests(sfd)
 
 
-## ----plot test characteristics, fig.height = 5-----------------------------------------------
+## ----plot test characteristics, fig.height = 5------------------------------------------------
 plot_priors_tests(sfd)
 
 
-## ----infection dynamics priors---------------------------------------------------------------
+## ----infection dynamics priors----------------------------------------------------------------
 sfd$inf_dyn_priors
 
 
-## ----set infection dynamics priors-----------------------------------------------------------
+## ----set infection dynamics priors------------------------------------------------------------
 sfd <- set_priors_inf_dyn(sfd,
                    pi1_a = 1,
                    pi1_b = 1,
@@ -120,76 +120,76 @@ sfd <- set_priors_inf_dyn(sfd,
                    tau2_b = 1.5)
 
 
-## ----show infection dynamics priors----------------------------------------------------------
+## ----show infection dynamics priors-----------------------------------------------------------
 show_inf_dyn(sfd)
 
 
-## ----plot infection dynamics priors, fig.height = 5------------------------------------------
+## ----plot infection dynamics priors, fig.height = 5-------------------------------------------
 plot_priors_inf_dyn(sfd)
 
 
-## ----JAGS model compilation, eval = FALSE----------------------------------------------------
-## compiled_model <- compile_JAGS(sfd,
-##                                n_chains = 4,
-##                                keep_model_file = TRUE)
+## ----JAGS model compilation, eval = TRUE------------------------------------------------------
+compiled_model <- compile_JAGS(sfd,
+                               n_chains = 4,
+                               keep_model_file = TRUE)
 
 
-## ----sampling from JAGS model, eval = FALSE--------------------------------------------------
-## samples <- sample_model(compiled_model,
-##                         n_burnin = 5000,
-##                         n_iter = 5000,
-##                         n_thin = 20)
+## ----sampling from JAGS model, eval = TRUE----------------------------------------------------
+samples <- sample_model(compiled_model,
+                        n_burnin = 5000,
+                        n_iter = 5000,
+                        n_thin = 20)
 
 
-## ---- eval = FALSE---------------------------------------------------------------------------
+## ---- eval = FALSE----------------------------------------------------------------------------
 ## str(samples, max.level = 1)
 
 
-## ----creates folder to store model output if not present, eval = FALSE, include = FALSE------
+## ----creates folder to store model output if not present, eval = FALSE, include = FALSE-------
 ## if(!"./model_output" %in% list.dirs()) dir.create("model_output")
 
 
-## ----writing JAGS output to disk, eval = FALSE-----------------------------------------------
+## ----writing JAGS output to disk, eval = FALSE------------------------------------------------
 ## write.csv(samples$parameters, "model_output/param.csv", row.names = FALSE)
 ## write.csv(samples$proba_inf, "model_output/proba_inf.csv", row.names = FALSE)
 ## write.csv(samples$Gelman_diag, "model_output/Gelman_diag.csv", row.names = FALSE)
 
 
-## ----Gelman-rubin diagnostics----------------------------------------------------------------
+## ----Gelman-rubin diagnostics-----------------------------------------------------------------
 Gelman_diag <- read.csv("model_output/Gelman_diag.csv")
 
 Gelman_diag
 
 
-## --------------------------------------------------------------------------------------------
+## ---------------------------------------------------------------------------------------------
 param <- as_tibble(read.csv("model_output/param.csv"))
 
 head(param)
 
 
-## ----parameter traceplots, fig.height = 5----------------------------------------------------
+## ----parameter traceplots, fig.height = 5-----------------------------------------------------
 ggplot(param, aes(x = .iteration, y = Se, col = factor(.chain))) +
   geom_line()
 
 
-## ----posterior parameter density, fig.height = 4.5-------------------------------------------
+## ----posterior parameter density, fig.height = 4.5--------------------------------------------
 ggplot(param, aes(x = Se, colour = "Posterior")) +
   stat_density(geom = "line") +
   geom_function(fun = dbeta, args = list(5000, 260), aes(colour = "Prior")) +
   scale_colour_discrete(name  ="")
 
 
-## --------------------------------------------------------------------------------------------
+## ---------------------------------------------------------------------------------------------
 param_summary(x = param)
 
 
-## ----loading predicted probabilities of infection--------------------------------------------
+## ----loading predicted probabilities of infection---------------------------------------------
 pinf <- as_tibble(read.csv("model_output/proba_inf.csv"))
 
 head(pinf)
 
 
-## ----plotting overall predicted probabilities of infection, fig.height = 5-------------------
+## ----plotting overall predicted probabilities of infection, fig.height = 5--------------------
 ggplot(pinf, aes(x = predicted_proba)) +
   geom_density()
 
@@ -200,73 +200,73 @@ ggplot(pinf, aes(x = predicted_proba, col = factor(herd_id))) +
   theme(legend.position = "none")
 
 
-## --------------------------------------------------------------------------------------------
+## ---------------------------------------------------------------------------------------------
 sfd$test_data %>%
   filter(herd_id == 29)
 
 
-## ----plotting predicted probabilities of infection for herd 29, fig.height = 4---------------
+## ----plotting predicted probabilities of infection for herd 29, fig.height = 4----------------
 pinf %>%
   filter(herd_id == 29) %>%
   ggplot(., aes(x = predicted_proba, col = factor(herd_id))) +
   geom_density()
 
 
-## --------------------------------------------------------------------------------------------
+## ---------------------------------------------------------------------------------------------
 sfd$test_data %>%
   filter(herd_id == 30)
 
 
-## ----plotting predicted probabilities of infection for herd 30, fig.height = 4---------------
+## ----plotting predicted probabilities of infection for herd 30, fig.height = 4----------------
 pinf %>%
   filter(herd_id == 30) %>%
   ggplot(., aes(x = predicted_proba, col = factor(herd_id))) +
   geom_density()
 
 
-## --------------------------------------------------------------------------------------------
+## ---------------------------------------------------------------------------------------------
 sfd$test_data %>%
   filter(herd_id == 68)
 
 
-## ----plotting predicted probabilities of infection for herd 68, fig.height = 4---------------
+## ----plotting predicted probabilities of infection for herd 68, fig.height = 4----------------
 pinf %>%
   filter(herd_id == 68) %>%
   ggplot(., aes(x = predicted_proba, col = factor(herd_id))) +
   geom_density()
 
 
-## --------------------------------------------------------------------------------------------
+## ---------------------------------------------------------------------------------------------
 sfd$test_data %>%
   filter(herd_id == 83)
 
 
-## ----plotting predicted probabilities of infection for herd 83, fig.height = 4---------------
+## ----plotting predicted probabilities of infection for herd 83, fig.height = 4----------------
 pinf %>%
   filter(herd_id == 83) %>%
   ggplot(., aes(x = predicted_proba, col = factor(herd_id))) +
   geom_density()
 
 
-## ----plotting predicted probabilities of infection for 4 herds, fig.height = 4---------------
+## ----plotting predicted probabilities of infection for 4 herds, fig.height = 4----------------
 pinf %>%
   filter(herd_id %in% c(29, 30, 68, 83)) %>%
   ggplot(., aes(x = predicted_proba, col = factor(herd_id))) +
   geom_density()
 
 
-## --------------------------------------------------------------------------------------------
+## ----summary of distributions for predicted probabilities of infection------------------------
 hrd_prob <- herd_summary(pinf, sfd, quantile = 0.5, cut_off = 0.5)
 
 head(hrd_prob)
 
 
-## ----intro dataset---------------------------------------------------------------------------
+## ----intro dataset----------------------------------------------------------------------------
 head(intro)
 
 
-## ----association cattle introductions and seroconversion-------------------------------------
-intro$ln_nAnim <- log(intro$nAnim)
+## ----association cattle introductions and seroconversion--------------------------------------
+intro$ln_nAnim <- log(intro$nAnim + 1)
 
 nAnim_lagged <- logit_nwinf_lagged(
   sf_data = sfd,
@@ -280,7 +280,7 @@ nAnim_lagged <- logit_nwinf_lagged(
 head(nAnim_lagged)
 
 
-## ---- plot of associations cattle introduced and seroconversion, fig.height = 4--------------
+## ---- plot of associations cattle introduced and seroconversion, fig.height = 4---------------
 ggplot(data = nAnim_lagged, aes(x = lag2, y = lag1, fill = AIC)) +
   geom_tile() +
   xlab("Time Lag 2 (months)") + ylab("Time Lag 1 (months)") +
@@ -288,7 +288,7 @@ ggplot(data = nAnim_lagged, aes(x = lag2, y = lag1, fill = AIC)) +
   ggtitle("Number of animals purchased")
 
 
-## ----association local seroprevalence seroconversion, warning = FALSE------------------------
+## ----association local seroprevalence seroconversion, warning = FALSE-------------------------
 lsprev <- logit_nwinf_lagged(
   sf_data = sfd,
   rf_data = bvd,
@@ -298,7 +298,7 @@ lsprev <- logit_nwinf_lagged(
   FUN = max)
 
 
-## ----plot association local seroprevalence seroconversion, echo= FALSE, fig.height = 4-------
+## ----plot association local seroprevalence seroconversion, echo= FALSE, fig.height = 4--------
 ggplot(data = lsprev, aes(x = lag2, y = lag1, fill = AIC)) +
   geom_tile() +
   xlab("Time Lag 2 (months)") +
@@ -307,16 +307,16 @@ ggplot(data = lsprev, aes(x = lag2, y = lag1, fill = AIC)) +
   ggtitle("Local seroprevalence")
 
 
-## --------------------------------------------------------------------------------------------
+## ----make nwinf-------------------------------------------------------------------------------
 nwinf <- make_nwinf_data(sfd,
                          time_of_inf = "mid")
 
 
-## --------------------------------------------------------------------------------------------
+## ----structure of nwinf-----------------------------------------------------------------------
 str(nwinf)
 
 
-## --------------------------------------------------------------------------------------------
+## ----add risk factors to STOCfree data--------------------------------------------------------
 nwinf <- add_risk_factor(nwinf,
                          intro,
                          rf_col = "ln_nAnim",
@@ -332,21 +332,21 @@ nwinf <- add_risk_factor(nwinf,
                          lag2 = 5)
 
 
-## --------------------------------------------------------------------------------------------
+## ---------------------------------------------------------------------------------------------
 str(nwinf)
 
 
-## ---- warning = FALSE------------------------------------------------------------------------
+## ---- warning = FALSE-------------------------------------------------------------------------
 modl <- logit_nwinf(nwinf,
                     risk_factors = c("ln_nAnim_8_8", 
                                      "LocalSeroPrev_5_5"))
 
 
-## --------------------------------------------------------------------------------------------
+## ---------------------------------------------------------------------------------------------
 summary(modl)
 
 
-## ----risk factor added to the STOCfree_data object-------------------------------------------
+## ----risk factor added to the STOCfree_data object--------------------------------------------
 sfd <- sf_add_risk_factor(
   sfd = sfd,
   risk_factor_data = intro,
@@ -359,10 +359,70 @@ sfd <- sf_add_risk_factor(
   FUN = sum)
 
 
-## ----tau1 discarded when risk factor included to STOCfree_data-------------------------------
+## ----tau1 discarded when risk factor included to STOCfree_data--------------------------------
 show_inf_dyn(sfd)
 
 
-## ----prior distribution for logistic regression----------------------------------------------
+## ----prior distribution for logistic regression-----------------------------------------------
 show_rf(sfd)
+
+
+## ---- echo = FALSE, fig.height = 5------------------------------------------------------------
+curve(invlogit(x),
+      from = -5, to = 5,
+      xlab = "logit(p)",
+      ylab = "p",
+      lwd = 2)
+grid()
+
+
+## ----set prior distributions risk factors-----------------------------------------------------
+sfd <- set_priors_rf(sfd,
+                   risk_factor = "Intercept",
+                   mean = -3, sd = 1)
+
+
+sfd <- set_priors_rf(sfd,
+                   risk_factor = "ln_nAnim_8_8",
+                   mean = 0, sd = 2)
+
+
+## ----display prior distributions risk factors-------------------------------------------------
+show_rf(sfd)
+
+
+## ----plot prior distributions risk factors, fig.height= 5-------------------------------------
+plot_priors_rf(sfd)
+
+
+## ----compiling JAGS model with risk factors---------------------------------------------------
+compiled_model1 <- compile_JAGS(sfd,
+                               n_chains = 4,
+                               keep_model_file = TRUE)
+
+
+## ----sampling from JAGS model with risk factors-----------------------------------------------
+samples <- sample_model(compiled_model1,
+                        n_burnin = 5000,
+                        n_iter = 5000,
+                        n_thin = 20)
+
+
+## ----saving results of model with risk factors------------------------------------------------
+write.csv(samples$parameters, "model_output/rf_param.csv", row.names = FALSE)
+write.csv(samples$proba_inf, "model_output/rf_proba_inf.csv", row.names = FALSE)
+write.csv(samples$Gelman_diag, "model_output/rf_Gelman_diag.csv", row.names = FALSE)
+
+
+## ----parameters of model with risk factors----------------------------------------------------
+param1 <- as_tibble(read.csv("model_output/rf_param.csv"))
+
+param_summary(x = param1)
+
+
+## ---- fig.height = 5--------------------------------------------------------------------------
+pinf1 <- as_tibble(read.csv("model_output/proba_inf.csv"))
+
+ggplot(pinf1, aes(x = predicted_proba)) +
+  geom_density()
 
